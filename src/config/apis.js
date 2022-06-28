3e67103bde9e08d44846b26116f1b810c298164d
Arpan Dhama <arpandhama2001@gmail.com>
2022-06-28 14:10:34 +0530

    sigle coin api added

--- src/config/apis.js
+++ src/config/apis.js
@@ -8,3 +8,7 @@ export const Trending = (currency) => {
 export const Top100 = (currency) => {
   return `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.toLowerCase()}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
 };
+
+export const coinData = (coinid) => {
+  return `https://api.coingecko.com/api/v3/coins/${coinid}`;
+};
